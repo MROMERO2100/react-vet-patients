5626e6be0bdc5c492de88fb14237a860c95cdd91
Moises Romero <mromero2100@gmail.com>
2022-12-14 16:52:50 -0600

    Update desde Visual Code Local

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-vet-patients
+Project of React with Vite for manage your Patients in the Veterinarian.
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <>
+            <h1>Vet Patient Manager</h1>
+        </>
+    )
+}
+
+export default Header;
